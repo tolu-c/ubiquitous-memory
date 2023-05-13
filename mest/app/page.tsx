729a9de3fc6cbc91e15811edb078e05c362af707
tolc-c <tolu.adeyemo7@gmail.com>
2023-05-13 21:44:12 +0100

    adjusted image size

--- mest/app/page.tsx
+++ mest/app/page.tsx
@@ -5,13 +5,16 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center min-h-full py-12 px-6 lg:px-8 bg-gray-100">
       <div className="mx-auto w-full max-w-md">
-        <Image
-          src="/images/logo.svg"
-          alt="mest logo image"
-          width={48}
-          height={48}
-          className="w-full mx-auto"
-        />
+        <div className="w-12 h-12 mx-auto">
+          <Image
+            src="/images/logo.svg"
+            alt="mest logo image"
+            width={480}
+            height={480}
+            className="w-max mx-auto"
+          />
+        </div>
+
         <h2 className="mt-6 text-center font-bold text-3xl tracking-tight text-gray-900">
           Sign in to your account
         </h2>

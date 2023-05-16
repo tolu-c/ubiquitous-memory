2f22512498b235b38a2630a6fed84ccb1ed6c5d1
tolc-c <tolu.adeyemo7@gmail.com>
2023-05-16 15:46:48 +0100

    app and prisma folders

---
+++ mest/app/types/types.ts
@@ -0,0 +1,44 @@
+import { ReactNode } from "react";
+
+export type AuthType = "LOGIN" | "REGISTER";
+
+export interface InputProps {
+  name: string;
+  type: "text" | "email" | "password";
+  onChange: (value: string, name: string) => void;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  icon?: {
+    src: ReactNode;
+  };
+  minLength?: number;
+  disabled?: boolean;
+}
+
+export interface FormData {
+  [key: string]: string;
+}
+export type Sizes = "xs" | "sm" | "md" | "lg" | "xl";
+
+export interface ButtonProps {
+  type: "button" | "submit";
+  title: string;
+  onClick?: () => void;
+  color?: "cta" | "primary" | "secondary";
+  size?: Sizes;
+  borderRadius?: Sizes;
+  icon?: {
+    src: ReactNode;
+    position: "left" | "right";
+    // color?: "primary" | "secondary" | "base";
+  };
+  variant?: "fill" | "outline" | "default" | "light";
+  compact?: boolean;
+  disabled?: boolean;
+}
+
+export interface AuthSocialButtonProps {
+  icon: ReactNode;
+  onClick: () => void;
+}
